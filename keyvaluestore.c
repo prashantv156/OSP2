d61e38c1ff69cdb582a349485ec50914ffbb0e9d
Ishan Deepak Munje <idmunje@ncsu.edu>
2016-10-24 02:00:28 -0400

    added get_keyvalue function

--- keyvaluestore.c
+++ keyvaluestore.c
@@ -7,13 +7,17 @@
 #include <asm/types.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
+
+#define DEBUG_MODE_1
+#define  MAX_NUMBER_OF_KEY_VALUE_PAIRS 2048
+
 
 pthread_cond_t empty, fill;
 pthread_mutex_t mutex;
 int done = 0;
 int transactionid = 0;
 
-int  MAX_NUMBER_OF_KEY_VALUE_PAIRS = 2048;
 
 
 static __u32 hash(__u64 key)
@@ -24,23 +28,27 @@ static __u32 hash(__u64 key)
 	return (key ^ (temp>>10) ^ (temp>>20)) & 0xBFF;
 }
 
-struct keyvalue_set
+struct keyvalue_base
 {
 	__u64 key;
 	__u64 size;
 	char data[1024];
+	// replace with char* data
+	// dont forget to allocate memory to data and copy cmd->kventry->data into it
 };
+typedef struct keyvalue_base keyvalue_set;
+typedef struct keyvalue_base keyvalue_get;
 
 struct dictnode 
 {
 	struct dictnode* next;
-	struct keyvalue_set* kventry;
+	keyvalue_set* kventry;
 	__u64 size; 
 };
 
 struct dictionary
 {
-	struct dictnode* hashmap[2048];
+	struct dictnode* hashmap[MAX_NUMBER_OF_KEY_VALUE_PAIRS];
 };
 
 struct dictionary* map = NULL;
@@ -49,25 +57,69 @@ struct dictionary* map = NULL;
 static void initializeDictionary()
 {
 	map = (struct dictionary*)malloc(sizeof(struct dictionary));	
+	if(map == NULL)
+	{
+		fprintf(stderr," Memory allocation to map failed!");
+		exit(0);
+	}
+	int i = 0;
+	for(i=0; i< MAX_NUMBER_OF_KEY_VALUE_PAIRS; i++)
+	{
+		map->hashmap[i]	= NULL;
+	}
+}
+
+static long get_keyvalue(keyvalue_get* cmd)
+{
+	int key_to_be_fetched	= cmd->key;
+	struct dictnode* val	= map->hashmap[key_to_be_fetched];
+
+	if(val == NULL)
+	{
+		#ifdef DEBUG_MODE_1
+			fprintf(stdout,"\tRequested key %llu is yet to be inserted in the map", cmd->key);
+		#endif
+		return -1;
+	}
+	else
+	{
+		while(val != NULL)
+		{
+			if(val->kventry->key	== cmd->key)
+			{
+				return 1;
+			}
+			val	= val->next;
+		}
+		
+		return -1;
+	}
+
 }
 
 
-static long keyvalue_set(struct keyvalue_set** cmd)
+static long set_keyvalue( keyvalue_set* cmd )
 {
 	
-	int key_to_be_set = (*cmd)->key;
-
-	struct dictnode* head = (struct dictnode*)malloc(sizeof(struct dictnode));
-	head->kventry = *cmd;
-	head->size = (*cmd)->size;
-	head->next = NULL;
-
+	int key_to_be_set = cmd->key;
 	struct dictnode* val = map->hashmap[key_to_be_set];
 	
-	if(val == 0)
+	if(val == NULL)
 	{
+	
+		struct dictnode* head = (struct dictnode*)malloc(sizeof(struct dictnode));
+		if(head == NULL)
+		{
+			fprintf(stderr," Memory allocation to node in dictionary failed!");
+			exit(0);
+		}
+		head->kventry = cmd;
+		head->size = cmd->size;
+		head->next = NULL;
+	
 		map->hashmap[key_to_be_set] = head;
 		return 1;
+
 	}
 	else
 	{	
@@ -75,11 +127,20 @@ static long keyvalue_set(struct keyvalue_set** cmd)
 		{	
 			if(val->kventry->key == key_to_be_set)
 			{	
-				strcpy(val->kventry->data, (*cmd)->data);
-				free(head);			
+				strcpy(val->kventry->data, (cmd)->data);
 			}
 			else
-			{
+			{	
+				struct dictnode* head = (struct dictnode*)malloc(sizeof(struct dictnode));
+				if(head == NULL)
+				{
+					fprintf(stderr," Memory allocation to node in dictionary failed!");
+					exit(0);
+				}
+				head->kventry = cmd;
+				head->size = cmd->size;
+				head->next = NULL;
+				
 				val->next = head;
 			}
 		}
@@ -87,18 +148,25 @@ static long keyvalue_set(struct keyvalue_set** cmd)
 		{
 			while(val->next != NULL)
 			{	
-
 				if(val->kventry->key == key_to_be_set)
 				{
-					strcpy(val->kventry->data,(*cmd)->data);
-					free(head);
+					strcpy(val->kventry->data,cmd->data);
 					return 1;
 				}								
 				val = val->next;	
 			}
 
-			val->next = head;
+			struct dictnode* head = (struct dictnode*)malloc(sizeof(struct dictnode));
+			if(head == NULL)
+			{
+				fprintf(stderr," Memory allocation to node in dictionary failed!");
+				exit(0);
+			}
+			head->kventry = cmd;
+			head->size = cmd->size;
+			head->next = NULL;
 		
+			val->next = head;
 		}
 		return 1;
 	}
@@ -117,10 +185,9 @@ void* consumer()
 	{
 		start = map->hashmap[i];
 		
-		if(start != 0)
+		if(start != NULL)
 		{
-			printf("start: %llu\n", start);
-						
+			printf("start: %llu\n", start->kventry->key);						
 			if(start->next == NULL)
 			{
 				printf("\nThe key is: %llu \n", start->kventry->key);
@@ -143,11 +210,12 @@ void* consumer()
 
 long kv_set(__u64 key, __u64 size, void *data)
 {
-     	struct keyvalue_set * cmd = (struct keyvalue_set *) malloc(sizeof(struct keyvalue_set));
+     	keyvalue_set * cmd = (keyvalue_set *) malloc(sizeof(keyvalue_set));
      	cmd->key = key;     
      	cmd->size = size;
      	strcpy(cmd->data, data);	
-     	return keyvalue_set(&cmd);
+     	//return set_keyvalue(&cmd);
+     	return set_keyvalue(cmd);
 }
 
 
@@ -169,7 +237,7 @@ void* producer()
         	a = rand();
        		sprintf(data,"%d",a);
 		tid = kv_set(i, strlen(data), data);
-        	fprintf(stderr,"S\t%d\t%d\t%d\t%s\n",tid,i,strlen(data),data);
+        	fprintf(stdout,"S\t%d\t%d\t%zu\t%s\n",tid,i,strlen(data),data);
     	}	
 	
 
@@ -180,6 +248,15 @@ int main()
 	printf("Begin\n");
 	pthread_t p;
 	pthread_t c;
+
+	pthread_create(&p, NULL, producer, NULL);
+	pthread_join(p, NULL);
+	printf("\n");
+	pthread_create(&p, NULL, consumer, NULL);
+	pthread_join(p, NULL);
+
+	printf("\nEnd\n");
+
 	return 0;
 
 }
